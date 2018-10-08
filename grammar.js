c6b350956ae6587839a630aec16bf1aec8b3f5f2
Jonas Damtoft <jonasdamtoft@hotmail.com>
2018-10-08 23:51:05 +0200

    adds literals

--- grammar.js
+++ grammar.js
@@ -6,10 +6,48 @@ module.exports = grammar({
   rules: {
     script: $ => seq(repeat($._global)),
     _global: $ => choice($.variable_declaration),
-    variable_declaration: $ => seq($.type_name, $.identifier, ";"),
-    type_name: $ =>
-      choice("integer", "float", "key", "string", "list", "vector", "rotation"),
+    _expression: $ => choice($._literal),
+
+    variable_declaration: $ =>
+      seq($.type_name, $.identifier, optional(seq("=", $._expression)), ";"),
+
+    // Literals
+    _literal: $ =>
+      choice(
+        $.integer_literal,
+        $.float_literal,
+        $.string_literal,
+        $.list_literal,
+        $.vector_literal,
+        $.quaternion_literal
+      ),
+    integer_literal: $ => /\d+/,
+    float_literal: $ => /(\d+\.\d*|\d*\.\d+)/,
+    string_literal: $ => seq('"', repeat(choice(/[^"\\]+/, /\\./)), '"'),
+    list_literal: $ => seq("[", commaSeparated($._expression), "]"),
+    vector_literal: $ =>
+      seq("<", $._expression, ",", $._expression, ",", $._expression, ">"),
+    quaternion_literal: $ =>
+      seq(
+        "<",
+        $._expression,
+        ",",
+        $._expression,
+        ",",
+        $._expression,
+        ",",
+        $._expression,
+        ">"
+      ),
+
     identifier: $ => /[_A-Za-z]\w*/,
-    comment: $ => /\/\/.*/
+    comment: $ => /\/\/.*/,
+
+    type_name: $ =>
+      choice("integer", "float", "key", "string", "list", "vector", "rotation")
   }
 });
+
+function commaSeparated(rule) {
+  return optional(seq(rule, repeat(seq(",", rule))));
+}
