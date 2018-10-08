39fd20a8985726ad4f6a6cdb4fb4236f50c952b4
Jonas Damtoft <jonasdamtoft@hotmail.com>
2018-10-08 21:32:25 +0200

    adds simple variable declaration

--- grammar.js
+++ grammar.js
@@ -4,7 +4,12 @@ module.exports = grammar({
   extras: $ => [$.comment, /\s/],
 
   rules: {
-    script: $ => seq(),
+    script: $ => seq(repeat($._global)),
+    _global: $ => choice($.variable_declaration),
+    variable_declaration: $ => seq($.type_name, $.identifier, ";"),
+    type_name: $ =>
+      choice("integer", "float", "key", "string", "list", "vector", "rotation"),
+    identifier: $ => /[_A-Za-z]\w*/,
     comment: $ => /\/\/.*/
   }
 });
