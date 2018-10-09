306b51caccb42cf50af1ea7068c82a5e20f264f6
Jonas Damtoft <jonasdamtoft@hotmail.com>
2018-10-09 14:57:09 +0200

    prepare for release

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 try {
   module.exports = require("./build/Release/tree_sitter_lsl_binding");
 } catch (error) {
-  try {
-    module.exports = require("./build/Debug/tree_sitter_lsl_binding");
-  } catch (_) {
-    throw error
-  }
+  throw error;
 }
